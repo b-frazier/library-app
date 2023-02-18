ae71047a9f3d73f3932136aaffeec190e74cf125
bfrazier <bthehippie2@gmail.com>
2023-02-18 17:18:55 -0500

    refactor for async/await

--- routes/api/authorsRoutes.js
+++ routes/api/authorsRoutes.js
@@ -14,21 +14,20 @@ router.get('/new', (req, res) => {
 });
 
 // create author
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render('authors/new', {
-        author: author,
-        errorMessage: 'Error creating author',
-      });
-    } else {
-      // res.redirect(`authors/${newAuthor.id}`)
-      res.redirect('authors');
-    }
-  });
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`authors/${newAuthor.id}`);
+    //     res.redirect('authors');
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating author',
+    });
+  }
 });
 
 module.exports = router;
