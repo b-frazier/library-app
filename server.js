a851ea1766b598854568c006bd31f5b950c5f55a
bfrazier <bthehippie2@gmail.com>
2023-02-18 14:17:31 -0500

    chnage process.env

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + 'public'));
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('--- connected to mongoose ---'));
