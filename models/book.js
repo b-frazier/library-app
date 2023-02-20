35993ef380881a22b4b1d01c2671183a6206f182
bfrazier <bthehippie2@gmail.com>
2023-02-20 10:38:33 -0500

    reference author

--- models/book.js
+++ models/book.js
@@ -25,6 +25,11 @@ const bookSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author',
+  },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
