485c999a9a9054bb6288b1fae30eb36beab3546b
bfrazier <bthehippie2@gmail.com>
2023-02-20 10:36:44 -0500

    add book route links

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const authors = require('./authorsRoutes');
+const books = require('./booksRoutes');
 
 router.use('/authors', authors);
+router.use('/books', books);
 
 module.exports = router;
